d6bdfabdd4b848e911a9510b0be8d658e5984096
lasha tinikashvili <lashatinikashvili29@gmail.com>
2026-03-03 17:09:06 +0400

    added working hours config

---
+++ BookingSystem.Persistence/Configurations/WorkingHoursConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using BookingSystem.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingSystem.Persistence.Configurations;
+
+public class WorkingHoursConfiguration : IEntityTypeConfiguration<WorkingHours>
+{
+    public void Configure(EntityTypeBuilder<WorkingHours> builder)
+    {
+        builder.ToTable("working_hours");
+
+        builder.HasKey(wh => wh.Id);
+
+        builder.Property(wh => wh.ProviderId)
+            .IsRequired();
+
+        builder.Property(wh => wh.DayOfWeek)
+            .IsRequired();
+
+        builder.Property(wh => wh.StartTime)
+            .IsRequired();
+
+        builder.Property(wh => wh.EndTime)
+            .IsRequired();
+
+        builder.Property(wh => wh.IsActive)
+            .IsRequired();
+
+        // Ensure that each provider can only have one entry per day of the week
+        builder.HasIndex(wh => new { wh.ProviderId, wh.DayOfWeek })
+            .IsUnique();
+    }
+}
