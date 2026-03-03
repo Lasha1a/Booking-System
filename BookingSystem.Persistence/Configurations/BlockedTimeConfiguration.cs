6e98bdad49874b7bb5aa9ad78c5917399f14ff59
lasha tinikashvili <lashatinikashvili29@gmail.com>
2026-03-03 17:36:59 +0400

    added blockedtimes config

---
+++ BookingSystem.Persistence/Configurations/BlockedTimeConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using BookingSystem.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingSystem.Persistence.Configurations;
+
+public class BlockedTimeConfiguration : IEntityTypeConfiguration<BlockedTime>
+{
+    public void Configure(EntityTypeBuilder<BlockedTime> builder)
+    {
+        builder.ToTable("blocked_times");
+
+        builder.HasKey(bt => bt.Id);
+
+        builder.Property(bt => bt.ProviderId)
+            .IsRequired();
+        builder.Property(bt => bt.StartDate)
+            .IsRequired();
+        builder.Property(bt => bt.EndDate)
+            .IsRequired();
+        builder.Property(bt => bt.Reason)
+            .HasMaxLength(500);
+        builder.Property(bt => bt.CreatedAt)
+            .IsRequired();
+    }
+}
