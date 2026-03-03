c4f8315b7f4bbb697cb03e2f0275bbaaab6885be
lasha tinikashvili <lashatinikashvili29@gmail.com>
2026-03-03 16:42:39 +0400

    added datacontext class

---
+++ BookingSystem.Persistence/Data/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using BookingSystem.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingSystem.Persistence.Data;
+
+public class DataContext(DbContextOptions<DataContext> options) : DbContext(options)
+{
+    public virtual DbSet<Appointment> Appointments { get; set; } 
+    public virtual DbSet<ServiceProvider> ServiceProviders { get; set; }
+    public virtual DbSet<WorkingHours> WorkingHours { get; set; }
+    public virtual DbSet<BlockedTime> BlockedTimes { get; set; }
+    public virtual DbSet<NotificationLog> NotificationLogs { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(DataContext).Assembly);
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
