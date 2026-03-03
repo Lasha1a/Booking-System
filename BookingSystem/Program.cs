ad8fdde1cc72afd6d944d16c25087dbd8e1812f2
lasha tinikashvili <lashatinikashvili29@gmail.com>
2026-03-03 14:20:05 +0400

    added scalar UI

--- BookingSystem/Program.cs
+++ BookingSystem/Program.cs
@@ -1,4 +1,5 @@
 using BookingSystem;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +17,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference(); //added scalar Ui
 }
 
 app.UseHttpsRedirection();
