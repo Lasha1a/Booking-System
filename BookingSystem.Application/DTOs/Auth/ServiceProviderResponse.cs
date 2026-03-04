25fc591dcaaf0bda5cf543eeda9ffbfa3e6e4dad
lasha tinikashvili <lashatinikashvili29@gmail.com>
2026-03-04 15:11:45 +0400

    added auth dtos and email specification

---
+++ BookingSystem.Application/DTOs/Auth/ServiceProviderResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingSystem.Application.DTOs.Auth;
+
+public class ServiceProviderResponse
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Specialty { get; set; } = string.Empty;
+    public bool IsActive { get; set; }
+}
---
+++ BookingSystem.Persistence/Specifications/Auth/ServiceProviderByEmailSpec.cs
@@ -0,0 +1,16 @@
+﻿using BookingSystem.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingSystem.Persistence.Specifications.Auth;
+
+public class ServiceProviderByEmailSpec : BaseSpecification<ServiceProvider>
+{
+
+    // finds specific serviceprovider for matched email
+    public ServiceProviderByEmailSpec(string email)
+        : base(sp => sp.Email.ToLower() == email.ToLower()) { }
+}
