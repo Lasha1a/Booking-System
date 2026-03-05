607ea6b2e591fe915dffb6bbb7ec563e40254a7d
lasha tinikashvili <lashatinikashvili29@gmail.com>
2026-03-05 16:04:17 +0400

    added encapsulation for workinghours entity

--- BookingSystem.Domain/Entities/WorkingHours.cs
+++ BookingSystem.Domain/Entities/WorkingHours.cs
@@ -9,12 +9,35 @@ namespace BookingSystem.Domain.Entities;
 
 public class WorkingHours : BaseEntity
 {
-    public Guid ProviderId { get; set; }
-    public DayOfWeek DayOfWeek { get; set; }
-    public TimeOnly StartTime { get; set; }
-    public TimeOnly EndTime { get; set; }
-    public bool IsActive { get; set; } = false;
+    public Guid ProviderId { get; private set; }
+    public DayOfWeek DayOfWeek { get; private set; }
+    public TimeOnly StartTime { get; private set; }
+    public TimeOnly EndTime { get; private set; }
+    public bool IsActive { get; private set; } = false;
 
-    public ServiceProvider Provider { get; set; } = null!;
+    public ServiceProvider Provider { get; private set; } = null!;
+
+    private WorkingHours() { }
+
+    public static WorkingHours Create(Guid providerId, DayOfWeek dayOfWeek, TimeOnly startTime, TimeOnly endTime, bool isActive)
+    {
+        return new WorkingHours()
+        {
+            ProviderId = providerId,
+            DayOfWeek = dayOfWeek,
+            StartTime = startTime,
+            EndTime = endTime,
+            IsActive = isActive
+        };
+    }
+
+    public void UpdateSchedule(TimeOnly startTime, TimeOnly endTime)
+    {
+        StartTime = startTime;
+        EndTime = endTime;
+    }
+
+    public void Activate() => IsActive = true;
+    public void Deactivate() => IsActive = false;
 
 }
