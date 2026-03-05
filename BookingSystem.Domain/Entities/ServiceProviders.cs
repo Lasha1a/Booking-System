c2987b5c1036e1056e1c2c4e9fd1a8335c473a4b
lasha tinikashvili <lashatinikashvili29@gmail.com>
2026-03-05 13:30:19 +0400

    changed serviceprovider entity by adding encapsulation

--- BookingSystem.Domain/Entities/ServiceProviders.cs
+++ BookingSystem.Domain/Entities/ServiceProviders.cs
@@ -1,6 +1,7 @@
 ﻿using BookingSystem.Domain.Common;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,15 +10,47 @@ namespace BookingSystem.Domain.Entities;
 
 public class ServiceProvider : BaseEntity
 {
-    public string Name { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty;
-    public string PasswordHash { get; set; } = string.Empty;
-    public string Specialty { get; set; } = string.Empty;
-    public bool IsActive { get; set; } = false;
-    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public string Name { get; private set; } = string.Empty;
+    public string Email { get; private set; } = string.Empty;
+    public string PasswordHash { get; private set; } = string.Empty;
+    public string Specialty { get; private set; } = string.Empty;
+    public bool IsActive { get; private set; } = false;
+    public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
 
     //navigation properties
-    public ICollection<WorkingHours> WorkingHours { get; set; } = new List<WorkingHours>();
-    public ICollection<Appointment> Appointments { get; set; } = new List<Appointment>();
-    public ICollection<BlockedTime> BlockedTimes { get; set; } = new List<BlockedTime>();
+    public ICollection<WorkingHours> WorkingHours { get; private set; } = new List<WorkingHours>();
+    public ICollection<Appointment> Appointments { get; private set; } = new List<Appointment>();
+    public ICollection<BlockedTime> BlockedTimes { get; private set; } = new List<BlockedTime>();
+
+    // EF core needs emtpy constructor
+    private ServiceProvider() { }
+
+    //static factory method 
+    public static ServiceProvider Create(string  name, string email, string passwordHash, string specialty)
+    {
+        return new ServiceProvider()
+        {
+            Name = name,
+            Email = email,
+            PasswordHash = passwordHash,
+            Specialty = specialty,
+            IsActive = true,
+            CreatedAt = DateTime.UtcNow,
+        };
+    }
+
+    //business methods
+    public void Activate() => IsActive = true;
+
+    public void Deactivate() => IsActive = false;
+
+    public void UpdateProfile(string name, string specialty)
+    {
+        Name = name;
+        Specialty = specialty;
+    }
+
+    public void UpdatePasswordHasher(string passwordHash) => 
+        PasswordHash = passwordHash;
+
 }
