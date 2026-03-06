54dec8f807e6e6ec7a7b254063ee337d90a66198
lasha tinikashvili <lashatinikashvili29@gmail.com>
2026-03-06 15:20:41 +0400

    added provider Dtos

---
+++ BookingSystem.Application/DTOs/Provider/UpdateProviderRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingSystem.Application.DTOs.Provider;
+
+public class UpdateProviderRequest
+{
+    public string Name { get; set; } = string.Empty;
+    public string Specialty { get; set; } = string.Empty;
+}
