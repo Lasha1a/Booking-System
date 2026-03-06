d609c17d7da9c483f1690f7654aa369f2ad1f88f
lasha tinikashvili <lashatinikashvili29@gmail.com>
2026-03-06 17:03:04 +0400

    added provider controller for updating and getting providers profile

---
+++ BookingSystem/Controllers/ProviderController.cs
@@ -0,0 +1,45 @@
+﻿using BookingSystem.Application.DTOs.Provider;
+using BookingSystem.Application.Interfaces.Provider;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace BookingSystem.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProviderController : ControllerBase
+{
+    private readonly IProviderService _providerService;
+
+    public ProviderController(IProviderService providerService)
+    {
+        _providerService = providerService;
+    }
+
+    private Guid GetProviderIdFromToken()
+    {
+        var providerIdClaim = User.FindFirst(JwtRegisteredClaimNames.Sub)?.Value;
+        if(providerIdClaim == null)
+        {
+            throw new UnauthorizedAccessException("Invalid token");
+        }
+        return Guid.Parse(providerIdClaim);
+    }
+
+    [HttpGet("profile")]
+    public async Task<IActionResult> GetProfile()
+    {
+        var providerId = GetProviderIdFromToken();
+        var provider = await _providerService.GetProfileAsync(providerId);
+        return Ok(provider);
+    }
+
+    [HttpPut("profile")]
+    public async Task<IActionResult> UpdateProfile([FromBody] UpdateProviderRequest request)
+    {
+        var providerId = GetProviderIdFromToken();
+        await _providerService.UpdateProfileAsync(providerId, request);
+        return Ok("Profile updated successfully");
+    }
+}
