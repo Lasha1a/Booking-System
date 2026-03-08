cb15a582265d9082f7d8399a1eb890d6dea14105
lasha tinikashvili <lashatinikashvili29@gmail.com>
2026-03-08 16:18:34 +0400

    added validators for blocked hours

---
+++ BookingSystem.Application/Validators/BlockedTimes/AddBlockedTimeRequestValidator.cs
@@ -0,0 +1,27 @@
+﻿using BookingSystem.Application.DTOs.BlockedTimes;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingSystem.Application.Validators.BlockedTimes;
+
+public class AddBlockedTimeRequestValidator : AbstractValidator<AddBlockedTimeRequest>
+{
+    public AddBlockedTimeRequestValidator()
+    {
+        RuleFor(x => x.StartDate)
+            .NotEmpty().WithMessage("Start date is required");
+
+        RuleFor(x => x.EndDate)
+            .NotEmpty().WithMessage("End date is required")
+            .GreaterThanOrEqualTo(x => x.StartDate)
+            .WithMessage("End date must be after or equal to start date");
+
+        RuleFor(x => x.Reason)
+            .NotEmpty().WithMessage("Reason is required")
+            .MaximumLength(500).WithMessage("Reason cannot exceed 500 characters");
+    }
+}
