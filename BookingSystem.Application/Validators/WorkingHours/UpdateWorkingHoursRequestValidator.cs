d99c3fc564c05bd274193a925efe9d992709b619
lasha tinikashvili <lashatinikashvili29@gmail.com>
2026-03-08 15:10:28 +0400

    added validators for working hours

---
+++ BookingSystem.Application/Validators/WorkingHours/UpdateWorkingHoursRequestValidator.cs
@@ -0,0 +1,22 @@
+﻿using BookingSystem.Application.DTOs.WorkingHours;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingSystem.Application.Validators.WorkingHours;
+
+public class UpdateWorkingHoursRequestValidator : AbstractValidator<UpdateWorkingHoursRequest>
+{
+    public UpdateWorkingHoursRequestValidator()
+    {
+        RuleFor(x => x.StartTime)
+            .NotEmpty().WithMessage("Start time is required");
+
+        RuleFor(x => x.EndTime)
+            .NotEmpty().WithMessage("End time is required")
+            .GreaterThan(x => x.StartTime).WithMessage("End time must be after start time");
+    }
+}
