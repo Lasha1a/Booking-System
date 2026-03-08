e00fef752375c4a6073a79feb9fbc7614d9776c6
lasha tinikashvili <lashatinikashvili29@gmail.com>
2026-03-08 15:08:28 +0400

    added working hours dtos

---
+++ BookingSystem.Application/DTOs/WorkingHours/AddWorkingHoursRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingSystem.Application.DTOs.WorkingHours;
+
+public class AddWorkingHoursRequest
+{
+    public DayOfWeek DayOfWeek { get; set; }
+    public TimeOnly StartTime { get; set; }
+    public TimeOnly EndTime { get; set; }
+    public bool IsActive { get; set; }
+}
---
+++ BookingSystem.Application/DTOs/WorkingHours/WorkingHoursResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingSystem.Application.DTOs.WorkingHours;
+
+public class WorkingHoursResponse
+{
+    public Guid Id { get; set; }
+    public DayOfWeek DayOfWeek { get; set; }
+    public TimeOnly StartTime { get; set; }
+    public TimeOnly EndTime { get; set; }
+    public bool IsActive { get; set; }
+}
