473de6eba5effd12c3c0bc9eb96a04d3ffa6d94d
lasha tinikashvili <lashatinikashvili29@gmail.com>
2026-03-08 22:53:56 +0400

    in datacontext in modelcreation added datetime that converts into utc in postgresql automatically after running the endpoint

--- BookingSystem.Persistence/Data/DataContext.cs
+++ BookingSystem.Persistence/Data/DataContext.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using System.Threading.Tasks;
 
 namespace BookingSystem.Persistence.Data;
@@ -19,6 +20,26 @@ public class DataContext(DbContextOptions<DataContext> options) : DbContext(opti
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(DataContext).Assembly);
+
+        //fixed postgresql timezones
+        foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+        {
+            foreach (var property in entityType.GetProperties())
+            {
+                if (property.ClrType == typeof(DateTime))
+                {
+                    property.SetValueConverter(new ValueConverter<DateTime, DateTime>(
+                        v => v.ToUniversalTime(),
+                        v => DateTime.SpecifyKind(v, DateTimeKind.Utc)));
+                }
+                else if (property.ClrType == typeof(DateTime?))
+                {
+                    property.SetValueConverter(new ValueConverter<DateTime?, DateTime?>(
+                        v => v.HasValue ? v.Value.ToUniversalTime() : v,
+                        v => v.HasValue ? DateTime.SpecifyKind(v.Value, DateTimeKind.Utc) : v));
+                }
+            }
+        }
 
         base.OnModelCreating(modelBuilder);
     }
